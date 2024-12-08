0a8a99724ee336bf64888428d88bf1d10a21f321
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-08 22:47:25 +0300

    Entities Created

---
+++ ChatGPTClone.Domain/Entities/ChatSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.Entities
+{
+    public sealed class ChatSession : EntityBase //seald class ile kalıtım alınmasını engelledik //miras vermeyecek bir class
+    {
+        public string Title { get; set; }
+
+        public GptModelType Model { get; set; }
+
+        //If the type is list in the entity then it is a ValueObject
+        public List<ChatThread> Threads { get; set; } = []; //new list anlamına geliyor
+        public Guid AppUserId { get; set; }
+
+        //If the type of the list is ICollection then it is an Entity
+        //publi ICollection<ChatMessage> ChatMessages { get; set; }
+
+    }
+}
