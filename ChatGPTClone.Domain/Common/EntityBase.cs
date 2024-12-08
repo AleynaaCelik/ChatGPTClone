5c3ff13f1bb4c35d9e50825a436d4e67ce8025e6
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-08 22:48:35 +0300

    EntityBase Created

---
+++ ChatGPTClone.Domain/Common/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.Common
+{
+    public abstract class EntityBase : IEntity, ICreatedByEntity, IModifiedByEntity //kod tekrarını önlemek için oluşturulmuş bir base class
+    {
+        public virtual Guid Id { get; set; } //unic olarak tanımlanmış bir id   //virtual: ezilebilir olmasını sağlar, özelleri ezerek kullanabiliriz
+
+        public virtual DateTimeOffset CreatedOn { get; set; } //kayıt ne zaman oluşturuldu
+
+        public virtual string CreatedByUserId { get; set; } //kayıt kim tarafından oluşturuldu
+
+
+        public virtual DateTimeOffset? ModifiedOn { get; set; } //kayıt ne zaman değiştirildi
+
+        public virtual string ModifiedByUserId { get; set; } //kayıt kim tarafından değiştirildi
+    }
+}
