2ecc19e7cbced15736a33fbc7dac8f75f465c77d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-09 23:52:28 +0300

    ChatMessage Created

---
+++ ChatGPTClone.Domain/ValueObjects/ChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using ChatGPTClone.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.ValueObjects
+{
+    public sealed class ChatMessage
+    {
+        public string Id { get; set; }
+
+        public GptModelType Model { get; set; }
+
+        public ChatMessageType Type { get; set; }
+
+        public string Content { get; set; } //içerik
+
+        public DateTimeOffset CreatedOn { get; set; } //oluşturulma tarihi
+    }
+}
