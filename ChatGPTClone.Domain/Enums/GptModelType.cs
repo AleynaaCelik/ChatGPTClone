cf7cc83f03e940f3a565c45a5d6df093b4d0ba8b
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-09 23:50:07 +0300

    GptModelType Created

---
+++ ChatGPTClone.Domain/Enums/GptModelType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.Enums
+{
+    public enum GptModelType
+    {
+        GPT4o = 1,
+        GPT4oMini = 2,
+        GPT4 = 3
+    }
+}
