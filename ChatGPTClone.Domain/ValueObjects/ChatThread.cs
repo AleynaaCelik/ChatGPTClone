c7cacb89e8bde6c396082feb80b1c575165c158d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-09 23:53:03 +0300

    ChatThread Created

---
+++ ChatGPTClone.Domain/ValueObjects/ChatThread.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.ValueObjects
+{
+    public sealed class ChatThread
+    {
+        public string Id { get; set; }
+
+        public List<ChatMessage> Messages { get; set; } = [];
+
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
