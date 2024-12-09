aad3e920ae853d888c1846dcae5ff9a4f5b7fefb
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-09 23:48:49 +0300

    ChatMessageType Created

---
+++ ChatGPTClone.Domain/Enums/ChatMessageType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.Enums
+{
+    public enum ChatMessageType //mesaj kim tarafından gönderildi
+    {
+        //The promp which is sent to the GPT model
+        System = 1,    //bota ait mesajlar
+
+        //The messahe which is sent by the user
+        User = 2,     //kullanıcıya ait mesajlar
+
+        //The message which is sent by the assistant
+        Assistant = 3     //asistana ait mesajlar
+    }
+}
