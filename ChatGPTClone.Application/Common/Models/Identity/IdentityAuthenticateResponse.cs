52045c003d0063943e911e9b0076f0c5656901de
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:21:59 +0300

    IdentityAuthenticateResponse Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityAuthenticateResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityAuthenticateResponse
+    {
+        public string Token { get; set; } //içinde ne zaman biteceği yazacağı için sadece token yazmamız yeterli
+    }
+}
