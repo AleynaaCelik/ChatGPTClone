9c55e20c049ed3038f167ae21a53df89537088be
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:29:53 +0300

    IdentityLoginResponse Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityLoginResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityLoginResponse
+    {
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiresAt { get; set; }
+        public IdentityLoginResponse(string token, DateTime expiresAt, string refreshToken, DateTime refreshTokenExpiresAt)
+        {
+            Token = token;
+            RefreshToken = refreshToken;
+            ExpiresAt = expiresAt;
+            RefreshTokenExpiresAt = refreshTokenExpiresAt;
+        }
+    }
+}
