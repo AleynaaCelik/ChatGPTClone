b1485f67f8b6ff482390f3e970bc38db179a3c0a
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:32:40 +0300

    IdentityRegisterResponse Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityRegisterResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    
+    public class IdentityRegisterResponse
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string EmailToken { get; set; }
+
+        public IdentityRegisterResponse(Guid id, string email, string emailToken)
+        {
+            Id = id;
+            Email = email;
+            EmailToken = emailToken;
+        }
+    }
+}
