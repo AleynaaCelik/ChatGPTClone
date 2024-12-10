bd74f529bf46f430fcbc498d0ffe38cdc6f5956d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:17:26 +0300

    ErrorDto Created

---
+++ ChatGPTClone.Application/Common/Models/Errors/ErrorDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Errors
+{
+    public class ErrorDto
+    {
+        //Email
+        public string PropertyName { get; set; }
+
+        //Email is required
+        //Email is not a valid email address
+        //Email is too long
+        public IReadOnlyList<string> Messages { get; set; }
+
+        public ErrorDto(string propertyName, List<string> messages)
+        {
+            PropertyName = propertyName;
+            Messages = messages;
+        }
+    }
+}
