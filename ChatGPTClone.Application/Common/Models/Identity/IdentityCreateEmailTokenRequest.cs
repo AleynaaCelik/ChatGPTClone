f27ff7bb55264cb6a0c82d603390400aee6d10fe
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:24:04 +0300

    IdentityCreateEmailTokenRequest Create

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityCreateEmailTokenRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityCreateEmailTokenRequest
+    {
+        public string Email { get; set; }
+
+        public IdentityCreateEmailTokenRequest(string email)
+        {
+            Email = email;
+        }
+    }
+}
