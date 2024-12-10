3189ee1a23f780043a135ef471b6b10649e9073e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:16:00 +0300

    EmailVerificationDto Created

---
+++ ChatGPTClone.Application/Common/Models/Email/EmailVerificationDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Email
+{
+    public class EmailVerificationDto
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+        // Parametreli yapıcı metot
+        public EmailVerificationDto(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+
+        // Parametresiz yapıcı metot
+        public EmailVerificationDto()
+        {
+        }
+    }
+}
