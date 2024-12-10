d57a30f950dbb74fc87ffca8922cf030619b7646
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:26:53 +0300

    IdentityLoginRequest Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityLoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityLoginRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public IdentityLoginRequest(string email, string password)
+        {
+            Email = email;
+
+            Password = password;
+        }
+    }
+}
