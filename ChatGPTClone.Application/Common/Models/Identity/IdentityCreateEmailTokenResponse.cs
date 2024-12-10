07a36e0c478ab40182942b045e592ad52bcc119f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:24:57 +0300

    IdentityCreateEmailTokenResponse Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityCreateEmailTokenResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityCreateEmailTokenResponse
+    {
+        public string Token { get; set; }
+
+        public IdentityCreateEmailTokenResponse(string token)
+        {
+            Token = token;
+        }
+    }
+}
