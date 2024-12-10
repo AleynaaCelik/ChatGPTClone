46c537e3299c4ecd9b6957a80e651b3e50fb40ee
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:33:15 +0300

    IdentityVerifyEmailRequest Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityVerifyEmailRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityVerifyEmailRequest
+    {
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+
+
+        public IdentityVerifyEmailRequest(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+    }
+}
