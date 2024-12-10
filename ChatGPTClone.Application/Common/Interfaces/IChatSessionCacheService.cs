d78f0344de49315d1e9f7023f451c011af826e65
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:08:16 +0300

    IChatSessionCacheService Created

---
+++ ChatGPTClone.Application/Common/Interfaces/IChatSessionCacheService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Interfaces
+{
+    public interface IChatSessionCacheService
+    {
+        Task<List<ChatSessionGetAllDto>> GetAllAsync(CancellationToken cancellationToken);
+
+        Task<ChatSessionGetByIdDto> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
+        void Remove(Guid id);
+    }
+}
