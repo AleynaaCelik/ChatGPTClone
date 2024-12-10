f3b4b920504ca84dcacf3f9b1d996f92b367c8a4
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:30:37 +0300

    IdentityRefreshTokenRequest Created

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityRefreshTokenRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityRefreshTokenRequest
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public IdentityRefreshTokenRequest(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
