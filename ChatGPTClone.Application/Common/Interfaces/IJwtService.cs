f622dfbcbd53070116a80b0adcd39a02e55e3fb3
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-10 17:13:36 +0300

    IJwtService Created

---
+++ ChatGPTClone.Application/Common/Interfaces/IJwtService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Interfaces
+{
+    public interface IJwtService
+    {
+        JwtGenerateTokenResponse GenerateToken(JwtGenerateTokenRequest request);
+
+        bool ValidateToken(string token);
+        Guid GetUserIdFromJwt(string token);
+
+
+    }
+}
