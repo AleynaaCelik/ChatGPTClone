c5bd1bd7c3c8cf2b1feefd9b436aaaf53ffddea9
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-12 22:28:19 +0300

    JwtGenerateTokenResponse Created

---
+++ ChatGPTClone.Application/Common/Models/Jwt/JwtGenerateTokenResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Jwt
+{
+    public class JwtGenerateTokenResponse
+    {
+        public string Token { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public JwtGenerateTokenResponse(string token, DateTime expiresAt)
+        {
+            Token = token;
+
+            ExpiresAt = expiresAt;
+        }
+    }
+}
