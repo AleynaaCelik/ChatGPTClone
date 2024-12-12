0b4a9e4e93c9ea3f21a1652d232ba0b42624872d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-12 22:26:52 +0300

    JwtGenerateTokenRequest Created

---
+++ ChatGPTClone.Application/Common/Models/Jwt/JwtGenerateTokenRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Models.Jwt
+{
+    public class JwtGenerateTokenRequest
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public IList<string> Roles { get; set; }
+
+        public JwtGenerateTokenRequest(Guid id, string email, IList<string> roles)
+        {
+            Id = id;
+            Email = email;
+            Roles = roles;
+        }
+    }
+}
