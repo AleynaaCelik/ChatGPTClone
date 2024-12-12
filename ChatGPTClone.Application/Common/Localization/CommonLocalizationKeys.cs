6e264d928d8eb4632dcc1086e2015b08d374c8a0
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-12 22:33:41 +0300

    ‎CommonLocalizationKeys Created

---
+++ ChatGPTClone.Application/Common/Localization/CommonLocalizationKeys.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Localization
+{
+    public static class CommonLocalizationKeys
+    {
+        //Genel doğrulama hatası durumunda fırlatılacak istisna
+        public static string GeneralValidationException => nameof(GeneralValidationException);
+
+        //Genel bir nesne bulunamadığında fırlatılacak istisna
+        public static string GeneralNotFoundException => nameof(GeneralNotFoundException);
+
+        //Genel bir sunucu hatası durumunda fırlatılacak istisna
+        public static string GeneralInternalServerException => nameof(GeneralInternalServerException);
+
+        //Zorunlu alan doğrulama hatası
+        public static string ValidationIsRequired => nameof(ValidationIsRequired);
+
+        //Geçersiz alan doğrulama hatası
+        public static string ValidationIsInvalid => nameof(ValidationIsInvalid);
+
+        //Belirli bir aralıkta olması gereken alan doğrulama hatası
+        public static string ValidationMustBeBetween => nameof(ValidationMustBeBetween);
+    }
+}
