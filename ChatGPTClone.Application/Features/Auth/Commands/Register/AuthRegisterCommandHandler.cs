bc8ff353000ef1d3bcf0079bfbff42f51d26a794
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-12 22:45:10 +0300

    AuthRegister Operations Created

---
+++ ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.Email;
+using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.Register
+{
+    public class AuthRegisterCommandHandler : IRequestHandler<AuthRegisterCommand, ResponseDto<AuthRegisterDto>>
+    {
+        private readonly IIdentityService _identityService;
+
+        private readonly IEmailService _emailService;
+        public AuthRegisterCommandHandler(IIdentityService identityService, IEmailService emailService)
+        {
+            _identityService = identityService;
+            _emailService = emailService;
+        }
+
+        public async Task<ResponseDto<AuthRegisterDto>> Handle(AuthRegisterCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.RegisterAsync(request.ToIdentityRegisterRequest(), cancellationToken);
+
+            await _emailService.EmailVerificationAsync(new EmailVerificationDto(response.Email, response.EmailToken), cancellationToken);
+
+            return new ResponseDto<AuthRegisterDto>(AuthRegisterDto.Create(response), "User registered successfully");
+        }
+    }
+}
---
+++ ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterDto.cs
@@ -0,0 +1,27 @@
+﻿using ChatGPTClone.Application.Common.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.Register
+{
+    public class AuthRegisterDto
+    {
+        public Guid UserId { get; set; }
+
+        public string EmailToken { get; set; }
+
+        public AuthRegisterDto(Guid userId, string emailToken)
+        {
+            UserId = userId;
+            EmailToken = emailToken;
+        }
+
+        public static AuthRegisterDto Create(IdentityRegisterResponse response)
+        {
+            return new AuthRegisterDto(response.Id, response.EmailToken);
+        }
+    }
+}
