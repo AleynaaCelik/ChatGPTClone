8b8b77566c41c0d51095e1a325635a5c21f8d3e3
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-13 23:21:49 +0300

    AuthReSendEmailVerificationEmail Operations Created

---
+++ ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommand.cs
@@ -0,0 +1,21 @@
+﻿using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail
+{
+    public class AuthReSendEmailVerificationEmailCommand : IRequest<ResponseDto<string>>
+    {
+        public string Email { get; set; }
+
+        public AuthReSendEmailVerificationEmailCommand(string email)
+        {
+            Email = email;
+        }
+
+    }
+}
