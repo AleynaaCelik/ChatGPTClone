a62c51b231d37ca01577ede31a61a4680e163ca5
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-15 23:17:37 +0300

    ChatSessionRemoveCommand Created

---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Remove/ChatSessionRemoveCommand.cs
@@ -0,0 +1,21 @@
+﻿using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Remove
+{
+    public class ChatSessionRemoveCommand : IRequest<ResponseDto<Guid>>
+    {
+        public Guid Id { get; set; }
+
+        public ChatSessionRemoveCommand(Guid id)
+        {
+            Id = id;
+        }
+
+    }
+}
