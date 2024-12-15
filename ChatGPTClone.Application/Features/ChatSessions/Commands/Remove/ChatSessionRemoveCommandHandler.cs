204958e49dbadf4190955afc858bfeca5e4ae667
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-15 23:18:22 +0300

    ChatSessionRemoveCommandHandler Created

---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Remove/ChatSessionRemoveCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Remove
+{
+    public class ChatSessionRemoveCommandHandler : IRequestHandler<ChatSessionRemoveCommand, ResponseDto<Guid>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IChatSessionCacheService _chatSessionCacheService;
+
+        public ChatSessionRemoveCommandHandler(IApplicationDbContext dbContext, ICurrentUserService currentUserService, IChatSessionCacheService chatSessionCacheService)
+        {
+            _dbContext = dbContext;
+            _chatSessionCacheService = chatSessionCacheService;
+        }
+
+        public async Task<ResponseDto<Guid>> Handle(ChatSessionRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var chatSession = await _dbContext
+                .ChatSessions
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            _dbContext.ChatSessions.Remove(chatSession);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            _chatSessionCacheService.Remove(request.Id);
+
+            return new ResponseDto<Guid>(chatSession.Id, "Chat session was deleted successfully.");
+        }
+    }
+}
