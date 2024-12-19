7b7f2e6031a1183dce2a6ade811e0bb18cdec611
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-19 22:32:33 +0300

    ApplicationDbContext Configuration created

---
+++ ChatGPTClon.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Authorization.Roles;
+using ChatGPTClone.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.PowerBI.Api.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.Emit;
+namespace ChatGPTClone.Persistence.Context;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser, Role, Guid, AppUserClaim, AppUserRole, AppUserLogin, RoleClaim, AppUserToken>
+{
+    public DbSet<ChatSession> ChatSessions { get; set; } //ChatSession tablosunu oluşturuyor
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //bu classın constructoru //base classın constructorunu çağırıyor
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder builder) //bu metod tabloları oluştururken tablolar arasındaki ilişkileri belirlemek için kullanılır
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ ChatGPTClon.Persistence/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Persistence.Context
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection")); //set edilen connection stringi kullanarak bir db context oluşturuyor
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
