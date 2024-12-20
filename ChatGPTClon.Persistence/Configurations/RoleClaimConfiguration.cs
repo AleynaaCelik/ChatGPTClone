a54dde37a357f06c52a1e905ad5553399b7d7d48
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-20 23:47:18 +0300

    RoleClaimConfiguration Created

---
+++ ChatGPTClon.Persistence/Configurations/RoleClaimConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Authorization.Roles;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Persistence.Configurations
+{
+    public class RoleClaimConfiguration : IEntityTypeConfiguration<RoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<RoleClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetRoleClaims table
+            builder.ToTable("RoleClaims");
+        }
+    }
+}
