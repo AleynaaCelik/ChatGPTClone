7d952ddd091176ece6901ce81e8d3c926ec17c16
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-20 23:49:27 +0300

    UserClaimConfiguration Created

---
+++ ChatGPTClon.Persistence/Configurations/UserClaimConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Persistence.Configurations
+{
+    public class UserClaimConfiguration : IEntityTypeConfiguration<AppUserClaim>
+    {
+        public void Configure(EntityTypeBuilder<AppUserClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(x => x.Id);
+
+            // Maps to the AspNetUserClaims table
+            builder.ToTable("UserClaims");
+        }
+    }
+}
