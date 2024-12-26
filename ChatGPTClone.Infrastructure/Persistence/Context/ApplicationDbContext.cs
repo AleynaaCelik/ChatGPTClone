6786a48ab3dbc8e385d3a678b70da31792c92f6c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-26 23:01:01 +0300

    ApplicationDbContext Created

---
+++ ChatGPTClone.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Domain.Entities;
+using ChatGPTClone.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Infrastructure.Persistence.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser, Role, Guid, AppUserClaim, AppUserRole, AppUserLogin, RoleClaim, AppUserToken>, IApplicationDbContext
+    {
+        public DbSet<ChatSession> ChatSessions { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+       
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // This method is used to configure the database schema
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
