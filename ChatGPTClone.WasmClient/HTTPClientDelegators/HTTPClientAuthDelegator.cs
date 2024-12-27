c8e4b56c49ec05831dda8ddbe09928ffcdf1e848
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-27 23:37:29 +0300

    HttpClientAuthDelegator Created

---
+++ ChatGPTClone.WasmClient/HTTPClientDelegators/HTTPClientAuthDelegator.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+using ChatGPTClone.Application.Features.Auth.Commands.Login;
+using System.Net.Http.Headers;
+
+namespace ChatGPTClone.WasmClient.HTTPClientDelegators
+{
+    public class HttpClientAuthDelegator : DelegatingHandler
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public HttpClientAuthDelegator(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authLoginDto = await _localStorage.GetItemAsync<AuthLoginDto>("user-token");
+
+            if (authLoginDto is not null && !string.IsNullOrEmpty(authLoginDto.Token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authLoginDto.Token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+
+            // var response = await base.SendAsync(request, cancellationToken);
+
+            // if (response.StatusCode == HttpStatusCode.Unauthorized)
+            // {
+            // }
+
+            // return response;
+        }
+    }
+}
