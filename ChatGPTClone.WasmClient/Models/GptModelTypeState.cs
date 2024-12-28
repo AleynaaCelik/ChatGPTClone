521a36af718a414e924efc00810ef1d58c0b2644
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-28 22:27:56 +0300

    GptModelTypeState Created

---
+++ ChatGPTClone.WasmClient/Models/GptModelTypeState.cs
@@ -0,0 +1,10 @@
+﻿using ChatGPTClone.Domain.Enums;
+
+namespace ChatGPTClone.WasmClient.Models
+{
+    public class GptModelTypeState
+    {
+        public GptModelType GptModelType { get; set; } = GptModelType.GPT4o;
+        public Action StateChanged { get; set; }
+    }
+}
