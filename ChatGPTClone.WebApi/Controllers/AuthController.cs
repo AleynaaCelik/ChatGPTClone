d99bea186196ee56de0a167e6691ceaf85bfdf3c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-02 23:50:13 +0300

    AuthController Added

---
+++ ChatGPTClone.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using ChatGPTClone.Application.Features.Auth.Commands.Login;
+using ChatGPTClone.Application.Features.Auth.Commands.RefreshToken;
+using ChatGPTClone.Application.Features.Auth.Commands.Register;
+using ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatGPTClone.WebApi.Controllers
+{
+    public class AuthController : ApiControllerBase
+    {
+        public AuthController(ISender mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AuthLoginCommand command, CancellationToken cancellationToken)
+            => Ok(await Mediatr.Send(command, cancellationToken));
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(AuthRegisterCommand command, CancellationToken cancellationToken)
+            => Ok(await Mediatr.Send(command, cancellationToken));
+
+        [HttpPost("verify-email")]
+        public async Task<IActionResult> VerifyEmail(AuthVerifyEmailCommand command, CancellationToken cancellationToken)
+            => Ok(await Mediatr.Send(command, cancellationToken));
+
+        [HttpPost("resend-email-verification")]
+        public async Task<IActionResult> ResendEmailVerification(AuthReSendEmailVerificationEmailCommand command, CancellationToken cancellationToken)
+            => Ok(await Mediatr.Send(command, cancellationToken));
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(AuthRefreshTokenCommand command, CancellationToken cancellationToken)
+            => Ok(await Mediatr.Send(command, cancellationToken));
+    }
+}
+
