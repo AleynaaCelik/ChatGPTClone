00dbb4ddb209b79d798b43798f42087a7f4013bd
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-03 22:01:26 +0300

    AuthVerifyEmail Operations Added

---
+++ ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommand.cs
@@ -0,0 +1,23 @@
+﻿using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+    public class AuthVerifyEmailCommand : IRequest<ResponseDto<string>>
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+        public AuthVerifyEmailCommand(string email, string token)
+        {
+            Email = email;
+            Token = token;
+
+        }
+    }
+}
---
+++ ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+    public class AuthVerifyEmailCommandValidator : AbstractValidator<AuthVerifyEmailCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthVerifyEmailCommandValidator(IIdentityService identityService)
+        {
+            _identityService = identityService;
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MustAsync(EmalExists)
+                .WithMessage("Email is not found.");
+
+            RuleFor(x => x.Token)
+                .NotEmpty()
+                .MinimumLength(20)
+                .WithMessage("Token is invalid.");
+        }
+
+        private Task<bool> EmalExists(string email, CancellationToken cancellationToken)
+        {
+            return _identityService.CheckEmailExistsAsync(email, cancellationToken);
+        }
+
+
+    }
+}
