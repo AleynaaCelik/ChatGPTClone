e5049b10a624de634cfc72174a6f65a9dbc6175d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-03 21:51:40 +0300

    ChatSessionGetAll Operations Added

---
+++ ChatGPTClone.Application/Features/ChatSessions/Queries/GetAll/ChatSessionGetAllQueryHandler.cs
@@ -0,0 +1,20 @@
+using ChatGPTClone.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll;
+public class ChatSessionGetAllQueryHandler : IRequestHandler<ChatSessionGetAllQuery, List<ChatSessionGetAllDto>>
+{
+	private readonly IChatSessionCacheService _chatSessionCacheService;
+
+	public ChatSessionGetAllQueryHandler(IChatSessionCacheService chatSessionCacheService)
+	{
+		_chatSessionCacheService = chatSessionCacheService;
+	}
+
+	public Task<List<ChatSessionGetAllDto>> Handle(ChatSessionGetAllQuery request, CancellationToken cancellationToken)
+	{
+		return _chatSessionCacheService.GetAllAsync(cancellationToken);
+	}
+}
